a9f2715a73d66c9bd95c86c521a83b2d3e393489
hsh <tmdgus2568@gmail.com>
2021-12-03 15:58:08 +0900

    init

---
+++ src/main/java/com/goodmeaning/GoodmeaningApplication.java
@@ -0,0 +1,13 @@
+package com.goodmeaning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodmeaningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodmeaningApplication.class, args);
+	}
+
+}
