010e564128398d7e9f40ebfbe59910bc48da38a4
choi9818 <chwechoi9818@naver.com>
2021-12-22 09:20:01 +0900

    cart-product 연동 전 commit3 add ProductService

--- src/main/java/com/goodmeaning/service/ProductService.java
+++ src/main/java/com/goodmeaning/service/ProductService.java
@@ -1,5 +1,17 @@
 package com.goodmeaning.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.goodmeaning.persistence.ProductRepository;
+import com.goodmeaning.vo.ProductVO;
+
+@Service
 public class ProductService {
 
+	@Autowired
+	ProductRepository prepo;
+	public ProductVO selectById(Long pid) {
+		return prepo.findById(pid).orElse(null);
+	}
 }
