1c07840fa98f0d4b1c1bf9092284866261653910
heymj61 <heymj61@gmail.com>
2021-12-23 17:37:56 +0900

    1223-2

--- src/main/java/com/goodmeaning/persistence/ProductOptionRepository.java
+++ src/main/java/com/goodmeaning/persistence/ProductOptionRepository.java
@@ -1,9 +1,14 @@
 package com.goodmeaning.persistence;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.goodmeaning.vo.ProductOptionVO;
+import com.goodmeaning.vo.ProductVO;
 
 public interface ProductOptionRepository extends CrudRepository<ProductOptionVO, Long> {
 
+	Optional<List<ProductOptionVO>> findByProductNum(ProductVO productVO);
 }
