8d7afb906910e248dca0305893961561084f3ce1
choi9818 <chwechoi9818@naver.com>
2022-01-06 15:39:35 +0900

    ordernow 로직 추가 및 코드 정리 commit2

--- src/main/java/com/goodmeaning/controller/OrderController.java
+++ src/main/java/com/goodmeaning/controller/OrderController.java
@@ -1,89 +1,82 @@
 package com.goodmeaning.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import java.util.Map;
-
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.goodmeaning.persistence.ProductOptionRepository;
 import com.goodmeaning.persistence.ProductRepository;
 import com.goodmeaning.persistence.UserRepository;
 import com.goodmeaning.service.OrderService;
+import com.goodmeaning.service.ProductService;
 import com.goodmeaning.vo.ProductOptionVO;
 import com.goodmeaning.vo.ProductVO;
 import com.goodmeaning.vo.UserVO;
+
 @Controller
 public class OrderController {
-	
+
 	@Autowired
 	OrderService orderService;
-	
+
+	@Autowired
+	ProductService productService;
+
 	@Autowired
 	UserRepository urepo;
-	
+
 	@Autowired
 	ProductRepository prepo;
 
 	@Autowired
 	ProductOptionRepository porepo;
-	
-	
-	@RequestMapping("/order")//브라우저에서 요청하는 코드
+
+	@RequestMapping("/order") // 브라우저에서 요청하는 코드
 	public String selectAll(Long[] cartNum, HttpSession session, Model model) {
-		
-		System.out.println( Arrays.toString(cartNum) );
-		
-		// session의 id
-//		UserVO user = urepo.findById("01011114444").get();
-//    	session.setAttribute("user", user);
-    	UserVO user = (UserVO)session.getAttribute("user");    
-    	
-		model.addAttribute("olist", orderService.findOrderList(cartNum));		
-		return "user/order/order";		
+
+		System.out.println(Arrays.toString(cartNum));
+		UserVO user = (UserVO) session.getAttribute("user");
+
+		model.addAttribute("olist", orderService.findOrderList(cartNum));
+		return "user/order/order";
 	}
-	
-	@PostMapping("/ordernow/")
-	public String goOrderNow(Long productNum, HttpSession session, Model model) {
-		ProductVO product = prepo.findById(productNum).orElse(null);
-		
-		List<ProductOptionVO> options = porepo.findByProductNum(product);
-		
-	
-		model.addAttribute("product", product);
-		model.addAttribute("options", options);
-		//model.addAttribute("productVO", productVO);
-		System.out.println("model"+product);
-		System.out.println("model"+options);
+
+	@GetMapping("/ordernow")	
+	public String goOrderNow(
+			@RequestParam(value = "optionnum[]", required = false) List<Long> optionnum,
+			@RequestParam(value = "opquantity[]", required = false) List<Integer> opquantity, Long pronum,
+			Integer proquantity, HttpSession session, Model model) {
+		System.out.println("pronum=" + pronum);
+		System.out.println("opquantity=" + opquantity);
 		UserVO user = (UserVO)session.getAttribute("user"); 
+		ProductVO product = productService.selectById(pronum);
+		List<ProductOptionVO> optionList = new ArrayList<>();
+		 
+		if (opquantity == null) {//옵션이 없을 때 상품에서 가격 가져옴
+				model.addAttribute("proquantity", proquantity);
+				System.out.println("proquantity=" + proquantity);
+		}else { 
+			
+	 
+			for (int i = 0; opquantity != null && i < opquantity.size(); i++) {
+				ProductOptionVO option = porepo.findById(optionnum.get(i)).get();
+				optionList.add(option);
+			}
+	
+		}
+		model.addAttribute("product", product);//productVO
+		model.addAttribute("optionList", optionList);//optionVO
+		model.addAttribute("opquantity", opquantity);
 		return "user/order/ordernow";
 	}
-	
-	
-	@RequestMapping(value = "/buyclick", method = RequestMethod.POST)
-	@ResponseBody
-	public void buying (@RequestParam(value="optionnum[]", required = false) List<String> optionnum
-						,@RequestParam(value="opquantity[]", required = false) List<String> opquantity
-						, String pronum
-						, String proquantity) {
-		
-		System.out.println(pronum);
-		System.out.println(proquantity);
-		System.out.println(optionnum);
-		System.out.println(opquantity);
-		
-		
-		
-	}
+
 }
